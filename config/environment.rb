23ca93166835a20a20ddd6b279f520249ade81f5
Simon Micheler <s.micheler@lboro.ac.uk>
2017-12-15 16:15:41 +0000

    Update3

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,22 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+#ActionMailer::Base.smtp_settings = {
+#  :address => 'smtp.sendgrid.net',
+#  :port => '587',
+#  :authentication => :plain,
+#  :user_name => ENV['apikey'],
+#  :password => ENV['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'],
+#  :domain => 'heroku.com',
+#  :enable_starttls_auto => true
+#}
+
 ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey',
+  :password => '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',
+  :domain => 'herokuapp.com',
   :address => 'smtp.sendgrid.net',
-  :port => '587',
+  :port => 587,
   :authentication => :plain,
-  :user_name => ENV['apikey'],
-  :password => ENV['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'],
-  :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
