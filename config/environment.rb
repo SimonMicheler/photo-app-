c8bc3802399a9adec3485f2c89a6be1f4090816f
Simon Micheler <s.micheler@lboro.ac.uk>
2017-12-15 15:42:49 +0000

    Update1

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
-  :port => '587',
+  :port => '25',
   :authentication => :plain,
   :user_name => ENV['apikey'],
   :password => ENV['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'],
