f78ca39b43de9eef801000d528760ba6369e335e
Simon Micheler <s.micheler@lboro.ac.uk>
2017-12-15 14:59:39 +0000

    Establishing email password confirmation

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['apikey'],
+  :password => ENV['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'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
